eb378d3dd51a03a5901cb1694df31f59706b6a1f
Matthew Stead <mattstead6@gmail.com>
2022-02-28 23:05:26 -0500

    commit tree 91bd9801ea3b62335cb156616982484d65ecfc17

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const driversInArray = drivers.filter((theDriver) => findMatching(theDriver))
+
+
+function findMatching(drivers,name){
+    if(drivers.name == name){
+        return name
+    } 
+    return name
+}
